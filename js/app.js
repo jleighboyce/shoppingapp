ede4b2d3af358ec8f3729667f25e60e92028b185
jleighboyce <jleighboyce@gmail.com>
2015-08-12 07:16:05 -0500

    enterfunction,clearboxonenter,buttonbluebordertakenoff

--- js/app.js
+++ js/app.js
@@ -3,27 +3,30 @@ $(document).ready(function () {
 	$('.btn').click(function() {
 		var add = $('#field1').val();
 		$('<li class="plusone"></li>').appendTo('#added').html('<input type="checkbox" id="listfield">&nbsp<span>' + add + '</span>&nbsp<input type="reset" value="x" id="right">');
-		})
-	})
+			$('#field1').val('');
+			$('#field1').focus();
+		});
+	
 	$('#field1').keydown(function (e) {
 		if(e.keyCode == 13) {
 			var add = $('#field1').val();
 			$('<li class="plusone"></li>').appendTo('#added').html('<input type="checkbox" id="listfield">&nbsp<span>' + add + '</span>&nbsp<input type="reset" value="x" id="right">');
+			e.preventDefault();
+			$('#field1').val('');
 		}
-	})
+	});
+
 	$(document).on('click', '#right', function() {
 		$(this).closest('li').fadeOut(300);
 		$(this).remove();
 	});
 
+});
 
 
 
 
-
-	//$('#right').click(function() {
-	//	$('.plusone').remove(this).html();
-	//})
+	
 
 
 	
